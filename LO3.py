0ec5ee7ce312e23702da2d1c82feef8e5bbae4b8
guru2566 <36722072+guru2566@users.noreply.github.com>
2018-02-23 16:13:55 +0530

    Add files via upload

--- LO3.py
+++ LO3.py
@@ -1,9 +1,9 @@
 a = int(input("Enter the first Number:"))
 b = int(input("Enter the second Number:"))
 c = int(input("Enter the third Number:"))
-if a>b and a>c:
-        print(a,"is the largest")
-elif b>a and b>c:
-        print(b,"is the largest")
+if a<b and a<c:
+        print(a,"is the Lowest")
+elif b<a and b<c:
+        print(b,"is the Lowest")
 else :
-        print(c,"is the largest")
+        print(c,"is the Lowest")
